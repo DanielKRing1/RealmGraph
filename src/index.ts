081a427e1992353d46cd62549eeec9f18337bc27
Daniel Ring <DanielKRing1@gmail.com>
2022-09-07 19:30:38 -0700

    %npm_config_m%

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import RealmGraphManager from './RealmGraphManager/realmGraphManager';
+
+export * from './RealmGraph/types';
+export * from './RealmGraphManager/types';
+
+export default RealmGraphManager;
