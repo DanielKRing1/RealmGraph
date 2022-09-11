80254dce36ed0cb876e6d929bfd6a1e04c9a3cdb
Daniel Ring <DanielKRing1@gmail.com>
2022-09-10 17:12:40 -0700

    %npm_config_m%

--- src/RealmGraphManager/types.ts
+++ src/RealmGraphManager/types.ts
@@ -1,7 +1,7 @@
 import { RealmGraph, RGCreateParams } from "../RealmGraph/types";
 
 export type RealmGraphManager = {
-    getGraph: (graphName: string) => RealmGraph | undefined;
+    getGraph: (graphName: string) => RealmGraph | never;
     createGraph: (params: RGCreateParams) => Promise<RealmGraph>;
     rmGraph: (graphName: string) => void;
     loadGraphs: (metaRealmPath: string, loadableRealmPath: string) => Promise<number>;
